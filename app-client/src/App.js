ca98f20b0b116400a25c73ff93fb7c3ab8fc255a
Alex Yovev <ayovev511@gmail.com>
2018-10-19 21:18:59 -0700

    first commit, testing with Heroku

---
+++ app-client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="main">
+          <p>Website Under Construction</p>
+          <p className="contained">For inquiries feel free to contact us at <a href="mailto:ayovev511@gmail.com">ayovev511@gmail.com</a> or <a href="mailto:jameschnebly@gmail.com">jameschnebly@gmail.com</a></p>
+        </div>
+        {/* <div className="footer">
+          Made with <i class="fa fa-coffee" aria-hidden="true"></i> and <i class="fa fa-code" aria-hidden="true"></i>
+        </div> */}
+      </div>
+    );
+  }
+}
+
+export default App;
